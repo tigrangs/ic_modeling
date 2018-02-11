23e9b3e937ea6fb16eb40eebf04ae0ae7d66ba08
programtg <programtg@yahoo.com>
2018-02-11 21:10:05 +0400

    updated layer.hpp

--- src/core/layer.hpp
+++ src/core/layer.hpp
@@ -13,6 +13,19 @@ class layer
 public:
     using cells =  std::vector<std::vector<cell*>>;
 
+public:
+    layer(int id, unsigned width, unsigned height);
+
+public:
+    int id() const;
+    unsigned width() const;
+    unsigned height() const;
+
+private:
+    int m_id;
+    unsigned m_width;
+    unsigned m_height;
+
 private:
     cells m_cells;
 };
